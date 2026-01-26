5a09fc21614d54a56d5bc7a5fb7bd8d8f5c30baa
samuelanyoneai <bsreyes.es@gmail.com>
2026-01-26 00:06:28 -0500

    fix: corregir E402 en create_superuser.py - Agregar # noqa: E402 al import de get_user_model - VALIDADO LOCALMENTE: 6/6 checks pasando - 0 errores flake8, 20/20 tests, formato correcto - Pipeline CI/CD completo funcionando

--- create_superuser.py
+++ create_superuser.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 """Script para crear superusuario en Docker."""
 import os
+
 import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seguros.settings")
 django.setup()
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model  # noqa: E402
 
 User = get_user_model()
 
